fd92e63cfe3d507bdfae527d70425530dd5a5983
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2024-12-21 18:43:34 +0530

    'razorpa'

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const Razorpay = require("razorpay");
 const crypto = require("crypto");
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,13 +21,15 @@ const razorpay = new Razorpay({
 
 // Route to create a Razorpay payment order
 app.post("/create-order", async (req, res) => {
-  const { amount, receiptId } = req.body;
-
-  try {
+  const { amount
+  // , receiptId 
+  } = req.body;
+  console.log(amount)
+  try { 
     const options = {
-      amount: amount * 100, // Amount in paise
+      amount: amount, // Amount in paise
       currency: "INR",
-      receipt: receiptId,
+      // receipt: receiptId,
     };
 
     const order = await razorpay.orders.create(options);
@@ -34,7 +38,7 @@ app.post("/create-order", async (req, res) => {
       order,
     });
   } catch (error) {
-    console.error("Error creating order:", error.message);
+    console.error("Error creating order:", error);
     res.status(500).json({
       success: false,
       message: "Error creating Razorpay order",
@@ -54,11 +58,13 @@ app.post("/verify-payment", (req, res) => {
       .digest("hex");
 
     if (generatedSignature === signature) {
+      console.log("Payment signature successfully");
       res.status(200).json({
         success: true,
         message: "Payment verified successfully",
       });
     } else {
+      console.log("Payment signature failed");
       res.status(400).json({
         success: false,
         message: "Invalid payment signature",
